b082d5edd8a00f0b674fde10fb3641013e97bb74
pradeep-git135 <pradeepcr135@gmail.com>
2022-03-18 17:32:23 +0530

    18/march/2022

---
+++ demoJavascript/dataType.js
@@ -0,0 +1,78 @@
+// var a = 10;
+// var b = 100.001;
+// console.log(typeof(a));
+// console.log(b);
+
+// var str = 'pradeep'
+
+// var str2 = "ty"
+
+// console.log(typeof(str));
+// console.log(typeof(str2));
+
+// var result = true
+// var result2 = false
+
+// console.log(result +"     "+result2);
+// console.log(typeof(result) +"     "+typeof(result2));
+
+// var info = undefined
+
+// console.log(info);
+// var a;
+// console.log(a);
+
+
+// var data = null
+
+// console.log(data);
+
+
+//0, undefined, null  boolean value is false
+// if (null) {
+//     console.log("value of 1 is true");
+// }
+// else{
+//     console.log("value of 1 is false");
+// }
+
+// if ("") {
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+// ==   and ===
+
+var a = 10;
+var b = "10"
+
+// if (a==b) {     // validates only the value of operands
+//     console.log("they are equal");
+// }
+// else{
+//     console.log("not equal");
+// }
+
+// if (a === b) {     // validates both  value and datatype of operands
+//     console.log("they are equal");
+// }
+// else{
+//     console.log("not equal");
+// }
+
+if (0 == false) {
+    console.log("equal");
+}else{
+    console.log("not equal");
+}
+
+if (0 === false) {
+    console.log("equal");
+}else{
+    console.log("not equal");
+}
+
+//undefined null
---
+++ demoJavascript/external.js
@@ -0,0 +1,25 @@
+console.log("this output statement is from external file");
+
+var a = 10;
+var b = 30;
+a*b
+if (condition) {
+    
+}
+
+// var 2x = 20
+
+var x2qw = 23;
+
+var _a = 78
+
+var al$ = 23;
+
+//  var a b = 19;  
+
+//  var y!y = "invalid "
+
+
+// var if = 100  error
+
+// document.write("this statement is from external and printing it on webpage")
